dfb144cf273e4a90010a78b739301d3e619a69cb
Pierre-Louis RENARD <danapal.renard@gmail.com>
2026-03-20 17:02:00 +0100

    etudiants et commencement pilotes

--- app/routes.php
+++ app/routes.php
@@ -29,7 +29,6 @@ return function (App $app) {
     $app->get('/souhait', [SouhaitController::class, 'souhait']);
     $app->get('/politique', [HomeController::class, 'politique']);
     $app->get('/role', [HomeController::class, 'role']);
-    $app->get('/gestion_etudiants', [EtudiantController::class, 'gestion_etudiants']);
     $app->get('/gestion_pilotes', [PiloteController::class, 'gestion_pilotes']);
     $app->get('/creation_compte', [HomeController::class, 'creation_compte']);
     $app->get('/mentions', [HomeController::class, 'mentions']);
@@ -38,14 +37,22 @@ return function (App $app) {
     $app->get('/deconnexion', [ConnexionController::class, 'deconnecter']);
     $app->get('/postule',  [PostuleController::class, 'afficher2']);
     $app->post('/postule', [PostuleController::class, 'traiter']);
+
     $app->get('/creation_entreprise', [CreationEntrepriseController::class, 'creation_entreprise'])->setName('creation_entreprise');
     $app->post('/creation_entreprise/ajouter', [CreationEntrepriseController::class, 'ajoute']);
     $app->get('/creation_entreprise/modifier/{id}', [CreationEntrepriseController::class, 'modifier'])->setName('modifier-entreprise');
     $app->post('/creation_entreprise/modifier/{id}', [CreationEntrepriseController::class, 'modifier']);
     $app->post('/creation_entreprise/supprimer/{id}', [CreationEntrepriseController::class, 'supprimer'])->setName('supprimer-entreprise');
+
     $app->get('/gestion_offres', [OffreController::class, 'gestion_offres'])->setName('gestion_offres');
     $app->post('/gestion_offres/ajouter', [OffreController::class, 'ajoute']);
     $app->get('/gestion_offres/modifier/{id}', [OffreController::class, 'modifier'])->setName('modifier-offres');
     $app->post('/gestion_offres/modifier/{id}', [OffreController::class, 'modifier']);
     $app->post('/gestion_offres/supprimer/{id}', [OffreController::class, 'supprimer'])->setName('supprimer-offres');
+
+    $app->get('/gestion_etudiants', [EtudiantController::class, 'gestion_etudiants'])->setName('gestion_etudiants');
+    $app->post('/gestion_etudiants/ajouter', [EtudiantController::class, 'ajoute']);
+    $app->get('/gestion_etudiants/modifier/{id}', [EtudiantController::class, 'modifier'])->setName('modifier-offres');
+    $app->post('/gestion_etudiants/modifier/{id}', [EtudiantController::class, 'modifier']);
+    $app->post('/gestion_etudiants/supprimer/{id}', [EtudiantController::class, 'supprimer'])->setName('supprimer-offres');
 };
---
+++ src/Domain/Pilote.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Domain;
+
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\Table;
+
+#[Entity, Table(name: 'pilotes')]
+class Pilote
+{
+    #[Id, Column(type: 'integer'), GeneratedValue(strategy: 'AUTO')]
+    private int $id;
+
+    #[Column(type: 'string', nullable: false)]
+    private string $nom;
+
+    #[Column(type: 'string', nullable: false)]
+    private string $prenom;
+
+    #[Column(type: 'string', nullable: false)]
+    private string $lieu;
+
+    #[Column(type: 'string', nullable: false)]
+    private string $email;
+
+    #[Column(name: 'created_at', type: 'datetimetz_immutable', nullable: false)]
+    private DateTimeImmutable $createdAt;
+
+    public function __construct(string $nom, string $prenom, string $lieu, string $email)
+    {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->lieu = $lieu;
+        $this->email = $email;
+        $this->createdAt = new DateTimeImmutable('now');
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    public function getDomaine(): string
+    {
+        return $this->prenom;
+    }
+
+    public function setDomaine(string $prenom): void
+    {
+        $this->prenom = $prenom;
+    }
+
+    public function getLieu(): string
+    {
+        return $this->lieu;
+    }
+
+    public function setLieu(string $lieu): void
+    {
+        $this->lieu = $lieu;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getCreatedAt(): DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+}
